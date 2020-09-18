bb42c752fcd9e707cdd7dfda1d3e22e5329a92e2
Hursh Gupta <me@gmail.com>
2020-09-18 18:43:31 +0530

    just lat

--- learn/sec.cpp
+++ learn/sec.cpp
@@ -2,8 +2,11 @@
 
 int main(){
     std::cout<<"Hello,world"<<std::endl;
-    std::cout<<"this is going to be awesome!";
+    std::cout<<"I am learning c++;";
+    //both std::endl and '\n' are used to break lines
     return 0;
+    /*to multi line comment stuff you just gotta
+     * do this mate and this just works awesome */
 }
 
 
---
+++ learn/third.cpp
@@ -0,0 +1,12 @@
+//variables
+
+#include <iostream>
+
+int main(){
+    int a = 1;
+    int b;
+    b = 30;
+    std::cout<<(a+b);
+}
+
+ 
